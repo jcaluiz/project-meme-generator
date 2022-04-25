4bc5bf16ad40d72212e76a940a2f1a201c0b1612
Luiz Junior <lacjunior129.la@gmail.com>
2022-04-25 20:37:27 -0300

    requisito 2 concluído

--- script.js
+++ script.js
@@ -5,17 +5,21 @@ let memeText = document.querySelector('#meme-text');
 memeImageContainer.appendChild(memeText);
 memeText.appendChild(textInMeme);
 let inputText = document.querySelector('#text-input');
-let inputFile = document.querySelector('#input-image');
+let inputFile = document.querySelector('#meme-insert');
 
 
 function getImagePreview(event) { //ref: https://youtu.be/_u_GiyTcsjA // vídeo me ajudou muito a solucionar a dificuldade de colocar uma imagem
   let image = URL.createObjectURL(event.target.files[0]);
   let newImg = document.createElement('img');
-  newImg.setAttribute('id', 'meme-image')
+  newImg.setAttribute('id', 'img-id')
   newImg.src = image;
   newImg.width = '300';
-  memeImageContainer.appendChild(newImg);
+  let elementContainerImg = document.createElement('div');
+  elementContainerImg.setAttribute('id', 'meme-image');
+  memeImageContainer.appendChild(elementContainerImg);
+  elementContainerImg.appendChild(newImg)
 }
+
 
 function textoEmTempoReal () {
   inputText.addEventListener('keyup', function() {
