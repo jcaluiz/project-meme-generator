530065e3d4a8cf003b38830337a258f1883bf014
Luiz Junior <lacjunior129.la@gmail.com>
2022-04-29 12:07:29 -0300

    requisito 7 concluído

--- script.js
+++ script.js
@@ -1,23 +1,26 @@
+// <---- Variáveis reutilizáveis ---->
+
 let memeImageContainer = document.querySelector('#meme-image-container');
 let textInMeme = document.createElement('p');
-// textInMeme.className = 'meme-text';
 let memeText = document.querySelector('#meme-text');
 memeImageContainer.appendChild(memeText);
 memeText.appendChild(textInMeme);
 let inputText = document.querySelector('#text-input');
 let inputFile = document.querySelector('#meme-insert');
+let newImg = document.createElement('img');
+let elementContainerImg = document.createElement('div');
+elementContainerImg.setAttribute('id', 'meme-image');
 
+// <---- Funções ---->
 
 function getImagePreview(event) { //ref: https://youtu.be/_u_GiyTcsjA // vídeo me ajudou muito a solucionar a dificuldade de colocar uma imagem
   let image = URL.createObjectURL(event.target.files[0]);
-  let newImg = document.createElement('img');
-  newImg.setAttribute('id', 'img-id')
+  newImg.setAttribute('id', 'img-id');
   newImg.src = image;
   newImg.width = '300';
-  let elementContainerImg = document.createElement('div');
-  elementContainerImg.setAttribute('id', 'meme-image');
+  newImg.height = '300';
   memeImageContainer.appendChild(elementContainerImg);
-  elementContainerImg.appendChild(newImg)
+  elementContainerImg.appendChild(newImg);
 }
 
 
@@ -38,15 +41,61 @@ function buttonBorder() {
   earth.style.backgroundColor = 'green';
 
   fire.addEventListener('click', function() {
-    memeImageContainer.style.border = "3px dashed red"
+    memeImageContainer.style.border = "3px dashed red";
   })
 
   water.addEventListener('click', function() {
-    memeImageContainer.style.border = "blue 5px double"
+    memeImageContainer.style.border = "blue 5px double";
   })
 
   earth.addEventListener('click', function() {
-    memeImageContainer.style.border = "groove green 6px"
+    memeImageContainer.style.border = "groove green 6px";
   })
 }
 buttonBorder();
+
+function memesProntos() {
+  let img1 = document.querySelector('#meme-1');
+  let img2 = document.querySelector('#meme-2');
+  let img3 = document.querySelector('#meme-3');
+  let img4 = document.querySelector('#meme-4');
+  
+  function memes1Prontos() {
+    newImg.src = "/imgs/meme1.png";
+    newImg.width = '300';
+    newImg.height = '300';
+    newImg.setAttribute('id', 'meme-image');
+    memeImageContainer.appendChild(newImg);
+  }
+  
+  function memes2Prontos() {
+    newImg.src = "/imgs/meme2.png";
+    newImg.width = '300';
+    newImg.height = '300';
+    newImg.setAttribute('id', 'meme-image');
+    memeImageContainer.appendChild(newImg);
+  }
+  
+  function memes3Prontos() {
+    newImg.src = "/imgs/meme3.png"
+    newImg.width = '300';
+    newImg.height = '300';
+    newImg.setAttribute('id', 'meme-image');
+    memeImageContainer.appendChild(newImg);
+  }
+  
+  function memes4Prontos() {
+    newImg.src = "/imgs/meme4.png";
+    newImg.width = '300';
+    newImg.height = '300';
+    newImg.setAttribute('id', 'meme-image')
+    memeImageContainer.appendChild(newImg);
+  }
+  
+  img1.addEventListener('click', memes1Prontos);
+  img2.addEventListener('click', memes2Prontos);
+  img3.addEventListener('click', memes3Prontos);
+  img4.addEventListener('click', memes4Prontos);
+
+}
+memesProntos();
